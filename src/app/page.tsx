5b5ba8ddc3786a5cdfa8a211e218911fa462dd8b
Sayyam Mehra <sayyammehra@gmail.com>
2024-05-15 23:43:17 +0530

    Added content in home page and authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import Image from 'next/image'
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
 import MaxWidthWrapper from '@/components/maxWidthWrapper'
+import Phone from '@/components/phone'
+import { Check, Star } from 'lucide-react'
 
 export default function Home() {
   return (
-    <div className="bg-slate-50 ">
+    <div className="bg-slate-50">
       <section>
         <MaxWidthWrapper className="pb-24 pt-10 lg:grid lg:grid-cols-3 lg:gap-x-0 lg:pt-24 lg:pb-52 sm:pb-32 xl:gap-x-8 xl:pt-32">
           <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
@@ -29,8 +32,78 @@ export default function Home() {
                 precious memories.{' '}
               </p>
               <ul className="mt-8 space-y-2 text-left font-medium flex flex-col items-center sm:items-start">
-                <div className="space-y-2"></div>
+                <div className="space-y-2">
+                  <li className="flex gap-1.5 items-center text-left">
+                    <Check className="h-5 w-5 shrink-0 text-purple-600" />
+                    High-quality, durable material
+                  </li>
+                  <li className="flex gap-1.5 items-center text-left">
+                    <Check className="h-5 w-5 shrink-0 text-purple-600" />5 year
+                    print guarantee
+                  </li>
+                  <li className="flex gap-1.5 items-center text-left">
+                    <Check className="h-5 w-5 shrink-0 text-purple-600" />
+                    Modern iPhone models supported
+                  </li>
+                </div>
               </ul>
+              <div className="mt-12 flex flex-col sm:flex-row items-center sm:items-start gap-5">
+                <div className="flex -space-x-4">
+                  <img
+                    className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100"
+                    src="/users/user-1.png"
+                    alt="iser image"
+                  />
+                  <img
+                    className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100"
+                    src="/users/user-2.png"
+                    alt="iser image"
+                  />
+
+                  <img
+                    className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100"
+                    src="/users/user-3.png"
+                    alt="iser image"
+                  />
+
+                  <img
+                    className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100"
+                    src="/users/user-4.jpg"
+                    alt="iser image"
+                  />
+                  <img
+                    className="inline-block object-cover h-10 w-10 rounded-full ring-2 ring-slate-100"
+                    src="/users/user-5.jpg"
+                    alt="iser image"
+                  />
+                </div>
+                <div className="flex flex-col justify-between items-center sm:items-start">
+                  <div className="flex gap-0.5">
+                    <Star className="h-5 w-5 text-purple-600 fill-purple-500" />
+                    <Star className="h-5 w-5 text-purple-600 fill-purple-500" />
+                    <Star className="h-5 w-5 text-purple-600 fill-purple-500" />
+                    <Star className="h-5 w-5 text-purple-600 fill-purple-500" />
+                    <Star className="h-5 w-5 text-purple-600 fill-purple-500" />
+                  </div>
+                  <p>
+                    <span className="font-semibold">1,250</span> happy customers
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-span-full lg:col-span-1 w-full flex justify-center px-8 sm:px-16 md:px-0 mt-32 lg:m-x-0 lg:mt-20 h-fit">
+            <div className="relative md:max-w-xl">
+              <img
+                src="/your-image.png"
+                alt="your image"
+                className="absolute w-40 lg:w-52 left-56 -top-20 select-none hidden sm:block lg:hidden xl:block"
+              />
+              <img
+                src="/line.png"
+                className="absolute w-20 -left-6 -bottom-6 select-none"
+              />
+              <Phone className="w-64" imgSrc="/testimonials/1.jpg" />
             </div>
           </div>
         </MaxWidthWrapper>
--- src/components/maxWidthWrapper.tsx
+++ src/components/maxWidthWrapper.tsx
@@ -11,7 +11,7 @@ const MaxWidthWrapper = ({
   return (
     <div
       className={cn(
-        'h-full w-full mx-auto max-w-xl px-2.5 md:px-20',
+        'h-full w-full mx-auto max-w-screen-xl px-2.5 md:px-20',
         className
       )}
     >
