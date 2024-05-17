45069473bc483bb7c6e51d938b068c20aafb5f41
Sayyam Mehra <sayyammehra@gmail.com>
2024-05-17 17:02:30 +0530

    Added hero section and animated review section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable @next/next/no-img-element */
+import { Icons } from '@/components/icons'
 import MaxWidthWrapper from '@/components/maxWidthWrapper'
 import Phone from '@/components/phone'
+import { Reviews } from '@/components/reviews'
 import { Check, Star } from 'lucide-react'
 
 export default function Home() {
@@ -107,6 +109,98 @@ export default function Home() {
             </div>
           </div>
         </MaxWidthWrapper>
+      </section>
+      {/* Value proposition section */}
+      <section className="bg-slate-100 py-24">
+        <MaxWidthWrapper className="flex flex-col items-center gap-16 sm:gap-32">
+          <div className="flex flex-col lg:flex-row items-center gap-4 sm:gap-6">
+            <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">
+              What our{' '}
+              <span className="relative px-2">
+                customers{' '}
+                <Icons.underline className="hidden sm:block pointer-events-none absolute inset-x-0 -bottom-6 text-purple-500" />
+              </span>{' '}
+              say
+            </h2>
+            <img src="/snake-2.png" className="w-24 order-0 lg:order-2" />
+          </div>
+
+          <div className="mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2 gap-y-16">
+            <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
+              <div className="flex gap-0.5 mb-2">
+                <Star className="h-5 w-5 text-purple-600 fill-purple-500" />
+                <Star className="h-5 w-5 text-purple-600 fill-purple-500" />
+                <Star className="h-5 w-5 text-purple-600 fill-purple-500" />
+                <Star className="h-5 w-5 text-purple-600 fill-purple-500" />
+                <Star className="h-5 w-5 text-purple-600 fill-purple-500" />
+              </div>
+              <div className="text-lg leading-8">
+                <p>
+                  The case feels durable and I even got compliments on design.
+                  Had the case for two and a half months now and{' '}
+                  <span className="p-0.5 bg-slate-800 text-white">
+                    the image is super clear
+                  </span>
+                  , on the case I had before, the image started fading into
+                  yellow-ish colour after couple weeks. Love it.
+                </p>
+              </div>
+              <div className="flex gap-4 mt-2">
+                <img
+                  className="rounded-full h-12 w-12 object-cover"
+                  src="/users/user-1.png"
+                  alt="user"
+                />
+                <div className="flex flex-col">
+                  <p className="font-semibold">John Doe</p>
+                  <div className="flex gap-1.5 items-center text-zinc-600">
+                    <Check className="h-4 w-4 stroke-[3px] text-purple-600" />
+                    <p className="text-sm">Verified Purchase</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
+              <div className="flex gap-0.5 mb-2">
+                <Star className="h-5 w-5 text-purple-600 fill-purple-500" />
+                <Star className="h-5 w-5 text-purple-600 fill-purple-500" />
+                <Star className="h-5 w-5 text-purple-600 fill-purple-500" />
+                <Star className="h-5 w-5 text-purple-600 fill-purple-500" />
+                <Star className="h-5 w-5 text-purple-600 fill-purple-500" />
+              </div>
+              <div className="text-lg leading-8">
+                <p>
+                  I usually keep my phone together with my keys in my pocket and
+                  that led to some pretty heavy scratchmarks on all of my last
+                  phone case. This one, besides a barely noticeable scratch on
+                  the corner,{' '}
+                  <span className="p-0.5 bg-slate-800 text-white">
+                    looks brand new after 6 months
+                  </span>
+                  , and I dig it.
+                </p>
+              </div>
+              <div className="flex gap-4 mt-2">
+                <img
+                  className="rounded-full h-12 w-12 object-cover"
+                  src="/users/user-4.jpg"
+                  alt="user"
+                />
+                <div className="flex flex-col">
+                  <p className="font-semibold">Jonathan</p>
+                  <div className="flex gap-1.5 items-center text-zinc-600">
+                    <Check className="h-4 w-4 stroke-[3px] text-purple-600" />
+                    <p className="text-sm">Verified Purchase</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </MaxWidthWrapper>
+
+        <div className="pt-16">
+          <Reviews />
+        </div>
       </section>
     </div>
   )
